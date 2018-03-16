ea26bf464d12075291bc15258c7c9a817658f19f
Keombre <keombre8@gmail.com>
2018-03-16 20:57:02 +0100

    Boilerplate setup

---
+++ src/main.ts
@@ -0,0 +1,78 @@
+const { app, BrowserWindow } = require('electron')
+const storage = require('electron-json-storage')
+const path = require('path')
+const url = require('url')
+
+let win: Electron.BrowserWindow
+
+if (process.argv.includes('help')) {
+  process.stdout.write(`Maturita GPJP
+
+Arguments:
+
+  full    - enable fullscreen
+  dev     - enable dev tools
+  clearDB - clear database (Warning! List of all used books will be lost!)
+	`)
+  app.exit()
+}
+
+function createWindow () {
+  let args = {
+    width: 768,
+    height: 500,
+    icon: 'app/main.ico',
+    frame: false,
+    minWidth: 768,
+    minHeight: 500,
+    title: 'Maturita GPJP',
+    fullscreen: false
+  }
+
+  if (process.argv.includes('clearDB'))
+    storage.clear()
+
+  if (process.argv.includes('full'))
+    args['fullscreen'] = true
+
+  win = new BrowserWindow(args)
+
+  win.setMenu(null)
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, './index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  if (process.argv.includes('dev'))
+    win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    app.quit()
+  })
+}
+
+// Enforce single app instance
+let shouldQuit = app.makeSingleInstance(_ => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (win) {
+    if (win.isMinimized())
+      win.restore()
+    win.focus()
+  }
+})
+
+if (shouldQuit)
+  app.quit()
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin')
+    app.quit()
+})
+
+app.on('activate', () => {
+  createWindow()
+})
